34c3eb808427813c0ce194bd29183f922b8f20a1
Cal__ Nutella on Toast __MaxD <126923185+SMCallan@users.noreply.github.com>
2023-10-24 13:04:41 +0100

    Initial commit

---
+++ src/main/java/com/csm/highscore/gamehighscore/GameHighScoreApplication.java
@@ -0,0 +1,13 @@
+package com.csm.highscore.gamehighscore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameHighScoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameHighScoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/csm/highscore/gamehighscore/GameHighScoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csm.highscore.gamehighscore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameHighScoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
