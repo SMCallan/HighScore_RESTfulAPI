3798a6963a1b654871eb5dadce4b7e611b0199b5
Cal__ Nutella on Toast __MaxD <126923185+SMCallan@users.noreply.github.com>
2023-10-24 13:19:44 +0100

    Completed HighScore model with JPA annotations, getters, and setters.

---
+++ src/main/java/model/HighScore.java
@@ -0,0 +1,41 @@
+package model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class HighScore {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String playerName;
+    private int score;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+// Getters and setters
+}
